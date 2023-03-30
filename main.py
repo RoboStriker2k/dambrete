c92d94099a4a29f6480cdedd458fc569a84ff340
RoboStriker2k <rlaimite@gmail.com>
2023-03-31 00:24:17 +0300

    start

---
+++ main.py
@@ -0,0 +1,29 @@
+# speletaju krasas
+#       R     G   B  
+white =(255,255,255)
+black =(0  ,  0,  0)
+
+
+
+class spele:
+    """
+    Speles
+    """
+    
+    
+    
+    
+class Galds:
+
+
+class kaulins:
+    def __init__(self, krasa,dama=False):
+        self.krasa=krasa
+        self.dama=dama
+        self.vertiba=1
+        
+    def dama(self):
+        self.dama=True
+        self.vertiba=2
+        
+        

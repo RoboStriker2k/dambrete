f7d7d0cba77cd06469d9541a95f69bfb2070b9d4
RoboStriker2k <rlaimite@gmail.com>
2023-03-31 12:29:05 +0300

    Speles Laukuma izveide

--- main.py
+++ main.py
@@ -4,51 +4,124 @@ from time import sleep
 
 # speletaju krasas
 #       R     G   B  
-white =(255,255,255)
-black =(0  ,  0,  0)
+Balts =(255,255,255)
+Melns =(0  ,  0,  0)
 #logu izmers
 width =800
 height=600
 
 # speles iestatijumi
+SpelesLaukumaIzmers=8
+
+
+# Virzienu definicija
+KA="KA" # kreisais augšejais
+LA="LA" # labais augšējais
+KZ="KZ" # Kreisais apakšejais/zemais
+LZ="LZ" # Labais apakšējais/zemais
+
+# PYGame iestatijumi
+pygame.init()
 screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption('Dambrete')
 clock=pygame.time.Clock()
 laukums=pygame.Surface((600,600)) # W H
 panelis2=pygame.Surface((200,600))
+font1=pygame.font.Font(None,50)   
+teksts=font1.render('Dambrete',False,'black')
+
 def main():
     Running= True
     while Running==True:
-        screen.blit(laukums,(0,0))
-        screen.blit(panelis2,(600,0))
-        laukums.fill('chocolate')
-        panelis2.fill('Red')
-        
+        grafika()
+        krasojums()
         pygame.display.update()    
         clock.tick(60)
+
         
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
                 Running= False
-       
+def krasojums():
+    kvadrataizmers=height/SpelesLaukumaIzmers
+    
+    
+    print(kvadrataizmers) 
+         
+        
+def grafika():
+    screen.blit(laukums,(0,0))
+    screen.blit(panelis2,(600,0))
+    laukums.fill('chocolate')
+    panelis2.fill('Red')
+    screen.blit(teksts,(600,10) )
         
 
+    
     
 class spele:
     """
     Speles sakums
 
     """
-    pass    
+    def Sakuma_izkartojums ():
+        pass
+        
     
     
     
 class Galds:
     def __init__(self):
-        pass
-
-
+        self.matrica=self.jaunaSpele()
+    def jaunaSpele(self):
+        #izveido jaunu laukumu
+        matrica=[[None]*SpelesLaukumaIzmers for i in range(SpelesLaukumaIzmers)]
+        #azipilda to ar kauliņiem
+        for x in range(SpelesLaukumaIzmers):
+            for y in range(SpelesLaukumaIzmers):    
+                if (x%2 !=0) and (y%2==0):
+                    matrica[y][x]=laucins(Balts,kaulins(Balts))
+                elif (x%2!=0) and (y%2 !=0):
+                    matrica[y][x]=laucins(Melns,kaulins(Melns))
+                elif (x%2 ==0) and (y%2!=0):
+                    matrica[y][x]=laucins(Balts,kaulins(Balts))
+                elif (x%2==0) and (y%2 ==0):
+                    matrica[y][x]=laucins(Melns,kaulins(Melns))    
+                    
+        return matrica            
+    def atgriezt_laukumu(self,galds):
+        laukumavertibas=[[None]*SpelesLaukumaIzmers]*SpelesLaukumaIzmers            
+        for x in range(SpelesLaukumaIzmers):
+            for y in range(SpelesLaukumaIzmers):
+                if galds[x][y].krasa==Balts:
+                    laukumavertibas  [x][y]="Balts"
+                elif galds[x][y].krasa==Melns:
+                    laukumavertibas  [x][y]="Melns"   
+                else:
+                    laukumavertibas  [x][y]="Tukšs"      
+        return laukumavertibas      
+    
+    def lokacija(self,x,y):
+        x=int(x)
+        y=int(y)
+        return self.matrica[x][y]
+    
+    def relativitate(self,virziens,x,y):
+        match virziens:
+            case "KZ":
+                return(x-1,y+1)
+            case "LZ":
+                return(x+1,y+1)
+            case "KA":
+                return(x-1,y-1)
+            case "LA":
+                return(x+1,y-1)
+            case _:
+                return 0                                                  
+    def apkartejie(self,x,y):
+          return [self.relativitate(KA,x,y),self.relativitate(LA,x,y),self.relativitate(KZ,x,y),self.relativitate(LZ,x,y)]
+######################################## Kaulina definicija ############################################
 class kaulins:
     def __init__(self, krasa,dama=False):
         self.krasa=krasa
@@ -58,7 +131,14 @@ class kaulins:
     def dama(self):
         self.dama=True
         self.vertiba=2
+######################################### Laucina definicija  ##########################################
+class laucins:
+    def __init__(self,krasa,aiznemts=None):
+        self.krasa=krasa
+        self.aiznemts=aiznemts
+               
         
+######################################## INICIALIZE MAIN FUNKCIJU #################################################         
 if __name__=="__main__":
     main()
     pass        

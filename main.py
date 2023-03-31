b31a0c291deb4c51d24047fa6ff092b1f30d4a19
RoboStriker2k <rlaimite@gmail.com>
2023-03-31 11:08:01 +0300

    background setup logam

--- main.py
+++ main.py
@@ -10,19 +10,28 @@ black =(0  ,  0,  0)
 width =800
 height=600
 
+# speles iestatijumi
+screen=pygame.display.set_mode((width,height))
+pygame.display.set_caption('Dambrete')
+clock=pygame.time.Clock()
+laukums=pygame.Surface((600,600)) # W H
+panelis2=pygame.Surface((200,600))
 def main():
     Running= True
     while Running==True:
-        screen=pygame.display.set_mode((width,height))
+        screen.blit(laukums,(0,0))
+        screen.blit(panelis2,(600,0))
+        laukums.fill('chocolate')
+        panelis2.fill('Red')
         
-        pygame.display.update()
+        pygame.display.update()    
+        clock.tick(60)
+        
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
                 Running= False
-            
-        
-        
+       
         
 
     

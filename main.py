a5a5de81f36c3f6c818c7a964fddf0075cb5bdb0
RoboStriker2k <rlaimite@gmail.com>
2023-03-31 10:49:18 +0300

    Loga izvedošana

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
+import pygame ,sys
+from pygame.locals import *
+from time import sleep
+
 # speletaju krasas
 #       R     G   B  
 white =(255,255,255)
 black =(0  ,  0,  0)
+#logu izmers
+width =800
+height=600
 
+def main():
+    Running= True
+    while Running==True:
+        screen=pygame.display.set_mode((width,height))
+        
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                Running= False
+            
+        
+        
+        
 
-
+    
 class spele:
     """
-    Speles
+    Speles sakums
+
     """
-    
+    pass    
     
     
     
 class Galds:
+    def __init__(self):
+        pass
 
 
 class kaulins:
@@ -26,4 +50,6 @@ class kaulins:
         self.dama=True
         self.vertiba=2
         
-        
+if __name__=="__main__":
+    main()
+    pass        

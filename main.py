311e2fed05838ac4afb6fe552e76480fb88df24b
RoboStriker2k <rlaimite@gmail.com>
2023-04-10 16:21:20 +0300

    izlabota izlabotā funkcija main ciklā

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def main():
                     vertiba = bots.VertejumsGalda(spele)
             else:
                     bots2 = Bots(spele2, (255,  255, 255), 8)
-                    bots.gajiens(spele)
+                    bots2.gajiens(spele)
                     vertiba = bots2.VertejumsGalda(spele)
             time.sleep(0.1)
             grf.update(spele)
